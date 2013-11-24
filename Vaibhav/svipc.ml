f855a998263a853e85f6f1f577528d415254be4a
vaibhavjag <vaibhavjag@gmail.com>
2013-11-24 13:17:33 -0500

    svipc

--- Vaibhav/svipc.ml
+++ Vaibhav/svipc.ml
@@ -19,9 +19,9 @@ let _ =
   match action with
 
   | Ast -> (*let listing = Ast.string_of_program (program)
-           in print_endline listing*)print_endline "Program has beed passed"
+           in print_endline listing*)print_endline "Program has beed parsed"
   | Interpret -> (*ignore (Interpret.run (List.rev program))*)print_endline "Not Implemented Yet"
-  | Bytecode -> print_endline "Not Implemented Yet"
-  | Compile -> print_endline "Not Implemented Yet"
+  | Bytecode -> print_endline "Program has been parsed"
+  | Compile -> print_endline "Program has been parsed"
 
 
