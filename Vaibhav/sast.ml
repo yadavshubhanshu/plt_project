802e57e88666ffa4e3c54acb64c6779f8718f40e
vaibhavjag <vaibhavjag@gmail.com>
2013-12-11 15:11:55 -0500

    semantics functioncall

--- Vaibhav/sast.ml
+++ Vaibhav/sast.ml
@@ -5,7 +5,7 @@ module NameMap = Map.Make(struct
   let compare x y = Pervasives.compare x y
 end)
 
-let rec check_expr varmap = (function
+let rec check_expr varmap fmap = (function
 | Strings(_) -> "string"
 | Integers(_) -> "int"
 | Floats(_) -> "float"
@@ -13,12 +13,17 @@ let rec check_expr varmap = (function
 | Id(id) -> if (NameMap.mem id varmap) then (NameMap.find id varmap) 
 			else ((print_string ("Undeclared variable "^id^".\n"));"void")
 | Assign(id,exp) ->  	if (NameMap.mem id varmap) then (let lhs = (NameMap.find id varmap) 
-							and rhs = (check_expr varmap exp)
+							and rhs = (check_expr varmap fmap exp)
 							in (if not(lhs = rhs) then (print_string ("Error : Variable "^id^" has type "^lhs^
 							" and RHS has type "^rhs^".\n")));lhs) 
 						else ((print_string ("Undeclared variable "^id^".\n"));"void")
-| Call(_) -> "void"
-| Binop(e1,o,e2) -> let t1 = check_expr varmap e1 and t2 = check_expr varmap e2 in
+| Call(id,actuals) -> let fdecl = (if NameMap.mem id fmap then NameMap.find id fmap else (print_string ("Undeclared function '"^id^"'.\n");{ 
+      rtype = "";
+      fname = "";
+      formals = [];
+      body = [] 
+      })) in fdecl.rtype
+| Binop(e1,o,e2) -> let t1 = check_expr varmap fmap e1 and t2 = check_expr varmap fmap e2 in
 					(match o with
 					|Add -> (if not(t1 = t2) then print_string ("Error in Binary operator '+'... expression on the left has type "^t1^" and expression on the right has type "^t2^".\n"));t1
 					|Sub -> (if not(t1 = t2) then print_string ("Error in Binary operator '-'... expression on the left has type "^t1^" and expression on the right has type "^t2^".\n"));t1
@@ -40,7 +45,7 @@ let check_program (fdecl_list,vdecl_list,errors) =
 	let globals = List.fold_left (fun varmap vdecl -> (match vdecl with
 		| Vassign(ty,id_list,expr) -> (List.fold_left (fun vmap id -> (if (NameMap.mem id vmap) 
 			then print_string ("ERROR : variable "^id^" has already been declared in this scope.\n") );
-			let rhs = (check_expr vmap expr) 
+			let rhs = (check_expr vmap NameMap.empty expr) 
 			in if ty = rhs then  (NameMap.add id ty vmap) 
 				else ((print_string ("Error : Variable "^id^" has type "^ty^" and RHS has type "^rhs^".\n")); vmap))
 				varmap id_list)
